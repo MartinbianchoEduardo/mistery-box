34a6aea9d1af45bac9cf9624f4a44fb6c7f79fd1
Eduardo Martinbiancho <martinbianchoeduardo@gmail.com>
2022-08-17 15:30:26 -0300

    add jwt auth

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from 'src/auth/auth.service';
+import { LocalAuthGuard } from 'src/auth/local-auth.guard';
+
 
 @Controller()
 export class AppController {
-  @UseGuards(AuthGuard('local'))
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
   @Post('auth/login')
   async login(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Prisma } from '@prisma/client';
 import { UserService } from '../user/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async validateUser(userInput: Prisma.UserWhereUniqueInput, pass: string): Promise<any> {
+  async validateUser(
+    userInput: Prisma.UserWhereUniqueInput,
+    pass: string,
+  ): Promise<any> {
     const user = await this.userService.findOne(userInput);
     if (user && user.password === pass) {
       const { password, ...result } = user;
       return result;
     }
     return null;
+  }
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
